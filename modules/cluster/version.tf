8958b3bce4f36555fbca78db98601e0202f7d7a3
Joost Döbken <joost@dobdata.nl>
2020-10-26 14:45:38 +0100

    Update version.tf

--- modules/cluster/version.tf
+++ modules/cluster/version.tf
@@ -3,7 +3,7 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     hcloud = {
-      source = "hetznercloud/hcloud"
+      source  = "hetznercloud/hcloud"
       version = "1.22.0"
     }
   }
