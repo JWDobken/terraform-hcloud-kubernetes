e3e56f2a4c3c6dc93bfba8d4af93cb91c5661dad
Joost Döbken <joost@dobken.nl>
2021-02-24 09:24:38 +0100

    Update kubectl.tf

--- modules/kubernetes/kubectl.tf
+++ modules/kubernetes/kubectl.tf
@@ -9,6 +9,7 @@ resource "null_resource" "kubectl" {
 
   provisioner "local-exec" {
     command = <<EOT
+      mkdir -p $HOME/.kube
       scp -oStrictHostKeyChecking=no \
         root@${local.master_ip}:/root/.kube/config \
         $HOME/.kube/${var.cluster_name}.conf
